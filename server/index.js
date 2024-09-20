a4fccd984b4c4fdef6dc24899468690145c0bec1
ts-31 <104221666+ts-31@users.noreply.github.com>
2024-09-20 17:11:25 +0530

    Socket connection is done

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const {  createServer } = require("node:http");
+const { Server } = require("socket.io")
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const connectToMongoDb = require("./db/db");
@@ -6,6 +8,14 @@ const authRoutes = require("./routes/authRoutes");
 const lendRoutes = require("./routes/clothesRoute");
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST"],  
+    credentials: true, 
+  }
+});
 
 app.use(cors());
 app.use(express.json());
@@ -16,7 +26,15 @@ app.use('/api/post', lendRoutes);
 const port = process.env.PORT || 5000;
 
 connectToMongoDb().then(() => {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log("A user connected");
+
+  socket.on('disconnect', () => {
+    console.log("A user disconneced");
   });
 });
