63971e3187f0f4f5f7be0507abba0507a79d5f9f
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-03-31 22:43:43 +0530

    Added condition on routes

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -1,21 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../main";
 
 function NavbarComponent() {
   const { username } = useContext(AuthContext);
+
   return (
-    // <Navbar expand="lg" className="bg-body-tertiary w-100 mt-3">
-    <Navbar
-      expand="lg"
-      className="bg-dark navbar-dark w-auto mt-3 mx-3 rounded-top-3"
-    >
+    <Navbar expand="lg" className="bg-dark navbar-dark w-auto mt-3 mx-3 rounded-top-3">
       <Container fluid>
         <Navbar.Brand as={Link} to="/">
           {username ? `Hello, ${username}` : "ClothesRental"}
@@ -26,18 +19,24 @@ function NavbarComponent() {
             <Nav.Link as={Link} to="/" className="ms-5">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/chat" className="ms-5">
-              Chat
-            </Nav.Link>
-            <Nav.Link as={Link} to="/login" className="ms-5">
-              Login
-            </Nav.Link>
-            <Nav.Link as={Link} to="/account" className="ms-5">
-              Account
-            </Nav.Link>
-            <Nav.Link as={Link} to="/lend" className="ms-5">
-              Lend
-            </Nav.Link>
+            {username && (
+              <>
+                <Nav.Link as={Link} to="/chat" className="ms-5">
+                  Chat
+                </Nav.Link>
+                <Nav.Link as={Link} to="/account" className="ms-5">
+                  Account
+                </Nav.Link>
+                <Nav.Link as={Link} to="/lend" className="ms-5">
+                  Lend
+                </Nav.Link>
+              </>
+            )}
+            {!username && (
+              <Nav.Link as={Link} to="/login" className="ms-5">
+                Login
+              </Nav.Link>
+            )}
             <Nav.Link as={Link} to="/rent" className="ms-5">
               Rent
             </Nav.Link>
