513f281167dba978a06ee4606cb3f216e40ca2d6
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-04-01 18:56:28 +0530

    Added vercel backend url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  API_URL: "http://localhost:5000",
+  // API_URL: "http://localhost:5000",
+  API_URL: "https://clothes-rental-system-server-git-main-tejas-projects-9e04ed37.vercel.app",
 };
 
 export default config;
