d1f82bc496f3bf4ff799d31445c62fed56ee937a
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-07-14 10:03:20 +0530

    Change backend url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 const config = {
   // API_URL: "http://localhost:5000",
-  API_URL: "https://clothes-rental-system-server-git-main-tejas-projects-9e04ed37.vercel.app",
+  API_URL: "https://clothes-rental-system-backend.onrender.com",
+  // API_URL: "https://clothes-rental-system-server-git-main-tejas-projects-9e04ed37.vercel.app",
 };
 
 export default config;
